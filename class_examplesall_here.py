51e50b4b6c0de77406efd89453ee82f8de0ae785
PemaTamang11 <kulchan.pema@gmail.com>
2023-09-25 10:15:58 +0545

    Assignment

---
+++ class_examplesall_here.py
@@ -0,0 +1,24 @@
+# functionexamples
+# def python_test():
+#     print("hello")
+#
+#
+# python_test()
+
+#example2
+def python_test(a,b):
+    print(a)
+    print(b)
+    sum=a+b
+    return sum
+output=python_test(3,b=5)
+print(output)
+
+#docstring
+"""
+tripe quotes are used for docstring , used to document python module, class, function, methd to undesrtand what it does 
+"""
+
+
+
+
--- flow_control/int_str_sperate_list.py
+++ flow_control/int_str_sperate_list.py
@@ -1,14 +1,43 @@
-ip_str = input("Enter multiple values separated by space: ")
-list_val = ip_str.split()
+# ip_str = input("Enter multiple values separated by space: ")
+# list_val = ip_str.split()
+#
+# integers = []
+# strings = []
+#
+# for item in list_val:
+#     if item.isdigit():
+#         integers.append(int(item))
+#     else:
+#         strings.append(item)
+#
+# print("Strings:", strings)
+# print("Integers:", integers)
+user_input = input("Enter multiple values seperated by a comma: ")
 
-integers = []
-strings = []
+values = user_input.split(",")
 
-for item in list_val:
-    if item.isdigit():
-        integers.append(int(item))
-    else:
-        strings.append(item)
+int_list = []
 
-print("Strings:", strings)
-print("Integers:", integers)
+float_list = []
+
+string_list = []
+
+for value in values:
+
+    try:
+        int_value = int(value)
+        int_list.append(int_value)
+    except ValueError:
+
+        try:
+            float_value = float(value)
+            float_list.append(float_value)
+        except ValueError:
+
+            string_list.append(value)
+
+print("Integer list: ", int_list)
+
+print("Float list: ", float_list)
+
+print("String list: ", string_list)
